d2799226b1afef57f52c938f72f6e6311e357968
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-01-02 22:47:01 +0900

    Create deleteFolder.php

---
+++ function/deleteFolder.php
@@ -0,0 +1,12 @@
+<?php
+  $dir = '../file/'.$_POST['folderName'];
+  try {
+      $files = array_diff(scandir($dir), array('.','..'));
+      foreach ($files as $file) {
+          (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+      }
+      rmdir($dir);
+      echo json_encode(["result"=>1, "folder"=>$_POST['folderName']]);
+  } catch (\Exception $e) {
+      echo json_encode(["result"=>0, "error"=>$e]);
+  }
