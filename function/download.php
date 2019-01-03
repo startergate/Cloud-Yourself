41f94616a1805f18874a87ea35911962fdd751a1
ryan1357 <42164084+ryan1357@users.noreply.github.com>
2019-01-03 17:19:37 +0900

    Update download.php

--- function/download.php
+++ function/download.php
@@ -1,1 +1,31 @@
 <?php // 다운로드 (Ajax)safdsdf
+  $dir = "../file/".$_POST['fileName'];
+  $check = file_exists($dir);
+  if($check)
+  {
+    function mb_basename($path) { return end(explode('/',$path)); }
+function utf2euc($str) { return iconv("UTF-8","cp949//IGNORE", $str); }
+function is_ie() {
+	if(!isset($_SERVER['HTTP_USER_AGENT']))return false;
+	if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) return true; // IE8
+	if(strpos($_SERVER['HTTP_USER_AGENT'], 'Windows NT 6.1') !== false) return true; // IE11
+	return false;
+}
+
+$filepath = $dir;
+$filesize = filesize($filepath);
+$filename = mb_basename($filepath);
+if( is_ie() ) $filename = utf2euc($filename);
+
+header("Pragma: public");
+header("Expires: 0");
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=\"$filename\"");
+header("Content-Transfer-Encoding: binary");
+header("Content-Length: $filesize");
+
+readfile($filepath);
+  }
+  else {
+
+  }
