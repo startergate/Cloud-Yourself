e44922effb87c542e472fa1f5bf01fc37e399f61
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-01-03 13:42:03 +0900

    .

--- cloud.php
+++ cloud.php
@@ -1,2 +1,49 @@
-<?php
-  // 파일목록열람
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="./css/Cloud.css">
+    </head>
+    <body>
+        <div class="bar">
+        <h1>
+            <a class="Drive Yourself" href="./cloud.php">
+                <span class="blind">Drive Yourself</span>
+            </a>
+        </h1>
+        <li id = "gnb-my-layer"  class="gnb-my-li, profile" style="display: inline-block;">
+            <div id = "gnb-my-namebox" class = "gnb-my-namebox">
+                <a class="gnb-my" href="javascript:;" onclick="gnbUserLayer.click.Toggle(); return false">
+                    <img id="gnb-profile-img" src="https://ssl.pstatic.net/static/common/myarea/myInfo.gif" alt="내 프로필 이미지" style="display: line-block;" width="25" height="25">
+                    <span id="gnb-profile-filter-mask" class="filter-mask" style="display: inline-block;"></span>
+                    <span id ="gnb-name1" class="gnb-name" style="font-size: 15spx; color: white">alzkzk</span>
+                    <em class="blind" style="display: none;">내정보 보기</em>
+                </a>
+            </div>
+        </li>
+    </div>
+
+    <div id="gnb-my-lyr" class="gnb-my-lyr">
+        <div class="gnb-my-cont">
+            <div class="gnb-my-area">
+                <span class="gnb-mask"></span>
+                <img src="https://ssl.pstatic.net/static/common/myarea/myInfo.gif" alt="프로필 이미지" width="50" height="50">
+            </div>
+            <div class="gnb-text-area">
+                <div>
+                    <p class="gnb-cont">
+                        <span>asdfasdf</span>
+                    </p>
+
+                    <a id="gnb-text-logout"class="gnb-log-button">
+                        <span class="gnb-logout-text">로그아웃</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div border="left">
+
+    </div>
+    </body>
+</html>
