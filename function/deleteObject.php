504bf471bbe3553d9de8609763cd26d07f309cb3
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-01-05 10:25:52 +0900

    파일 삭제 추가

--- function/deleteObject.php
+++ function/deleteObject.php
@@ -3,12 +3,17 @@
   $dir = '../file/'.$_POST['folderName'];
 
   try {
+      (is_dir($dir)) ? delTree($dir) : unlink($dir);
+      echo json_encode(['result'=>1, 'target'=>$_POST['folderName']]);
+  } catch (\Exception $e) {
+      echo json_encode(['result'=>0, 'error'=>$e]);
+  }
+
+  function delTree($dir)
+  {
       $files = array_diff(scandir($dir), ['.', '..']);
       foreach ($files as $file) {
           (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
       }
       rmdir($dir);
-      echo json_encode(['result'=>1, 'folder'=>$_POST['folderName']]);
-  } catch (\Exception $e) {
-      echo json_encode(['result'=>0, 'error'=>$e]);
   }
